c1ac263f5a9ca9d6aaaf51eb87f80ae1e627eb68
rgirish2 <rgirish2@users.noreply.github.com>
2014-09-19 22:00:35 +0000

    Minimal gui. No intraction possible.

--- rgirish2/Assignment1.1/CS242/src/com/rgirish2/assignment1/gui/EightByEightBoardGUI.java
+++ rgirish2/Assignment1.1/CS242/src/com/rgirish2/assignment1/gui/EightByEightBoardGUI.java
@@ -1,5 +1,60 @@
 package com.rgirish2.assignment1.gui;
 
-public class EightByEightBoardGUI {
+import java.awt.FlowLayout;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.JFrame;
+import javax.swing.JTable;
+
+import com.rgirish2.assignment1.board.AbstractTwoDimensionalBoard;
+import com.rgirish2.assignment1.board.EightByEightBoard;
+import com.rgirish2.assignment1.piece.AbstractPieceSet;
+import com.rgirish2.assignment1.player.AbstractPlayer;
+import com.rgirish2.assignment1.player.NormalChessPlayer;
+
+@SuppressWarnings("serial")
+public class EightByEightBoardGUI extends JFrame {
+	private JFrame board;
+	
+	public EightByEightBoardGUI() {
+        this.board = new JFrame("8 X 8 Chess Board");
+        
+        this.board.setSize(800, 800);
+        
+        this.board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        this.board.setLayout(new FlowLayout());
+	}
+	
+	public void setChessPieces(AbstractTwoDimensionalBoard board) {
+        System.out.println("New Chess Board");
+        
+        String [] columnNames = {"1", "2", "3", "4", "5", "6", "7", "8"};
+        
+        JTable jTable = new JTable(board.getBoard(), columnNames);
+        
+        this.board.add(jTable);
+        
+        this.board.setVisible(true);
+	}
+	
+	public static void main(String [] args) {
+		int playerCount = 2;
+		
+		AbstractPlayer [] players = new NormalChessPlayer[playerCount];
+		AbstractTwoDimensionalBoard chessBoard = new EightByEightBoard();
+		List<AbstractPieceSet> allPieceSet = new ArrayList<AbstractPieceSet>();
+		
+		for (int i = 0; i < playerCount; i++) {
+			players[i] = new NormalChessPlayer("", i + 1);
+			allPieceSet.add(players[i].getPieceSet());
+		}
+		
+		chessBoard.buildBoard(allPieceSet);
+		
+		EightByEightBoardGUI chessBoardGUI = new EightByEightBoardGUI();
+		
+		chessBoardGUI.setChessPieces(chessBoard);
+	}
 }
