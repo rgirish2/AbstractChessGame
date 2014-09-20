f22849bc6f5aa30c0274d7cffaa0ab70fdf95453
rgirish2 <rgirish2@707f126f-a9f7-4686-a90d-106ec7087bbd>
2014-09-20 01:17:29 +0000

    Gui implementation by unicode characters.

---
+++ rgirish2/Assignment1.1/CS242/src/com/rgirish2/assignment1/movementValidity/Movements.java
@@ -0,0 +1,25 @@
+package com.rgirish2.assignment1.movementValidity;
+
+import com.rgirish2.assignment1.board.AbstractTwoDimensionalBoard;
+
+/**
+ * An class representing all the possible movements in a
+ * two dimensional square tiles boards.
+ * This is used to validate all the movements in a 2-D space.
+ * @author Rishi Girish (rgirish2ATillinoisDOTedu)
+ *
+ */
+public class Movements {
+	
+	public static boolean isValidVerticalMovement (AbstractTwoDimensionalBoard board, int curPosX, int curPosY, int newPosX, int newPosY) {
+		return false;
+	}
+
+	public static boolean isValidHorizontalMovement (AbstractTwoDimensionalBoard board, int curPosX, int curPosY, int newPosX, int newPosY)  {
+		return false;
+	}
+	
+	public static boolean isValidDiagonalMovement (AbstractTwoDimensionalBoard board, int curPosX, int curPosY, int newPosX, int newPosY) {
+		return false;
+	}
+}
